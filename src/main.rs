674a6940cdf7776c51cf2c3ba25eabed4f2741c9
Or Gany <gangana3@gmail.com>
2022-09-23 14:58:17 +0300

    Adding gl initialization, and handling the resize window event

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,15 @@
+extern crate gl;
 extern crate sdl2;
 
-use sdl2::event::Event;
+use sdl2::event::{Event, WindowEvent};
 use std::process::exit;
 use sdl2::video::Window;
+use gl::types::*;
+use sdl2::VideoSubsystem;
+use std::ffi::c_void;
+
+const INITIAL_WINDOW_WIDTH: u32 = 800;
+const INITIAL_WINDOW_HEIGHT: u32 = 600;
 
 fn start_event_loop(sdl_context: &sdl2::Sdl, window: &Window) {
     let mut event_pump = sdl_context.event_pump().unwrap();
@@ -11,6 +18,12 @@ fn start_event_loop(sdl_context: &sdl2::Sdl, window: &Window) {
             match event {
                 Event::Quit { .. } => {
                     exit(0);
+                }
+                Event::Window { win_event, .. } => {
+                    if let WindowEvent::Resized(width, height) = win_event {
+                        println!("Resizing!!!");
+                        unsafe { gl::Viewport(0, 0, width, height) }
+                    }
                 }
                 _ => { println!("Unhandled event event.") }
             }
@@ -21,11 +34,38 @@ fn start_event_loop(sdl_context: &sdl2::Sdl, window: &Window) {
 
 fn open_window(sdl_context: &sdl2::Sdl) -> Window {
     let video = sdl_context.video().unwrap();
-    video.window("My first OpenGL App!", 800, 600).resizable().build().unwrap()
+    video.window("My first OpenGL App!", INITIAL_WINDOW_WIDTH, INITIAL_WINDOW_HEIGHT)
+        .resizable()
+        .opengl()
+        .build()
+        .unwrap()
+}
+
+/// Prepare everything needed before we can start rendering some sh*t
+fn initialize_gl(sdl_context: &sdl2::Sdl) {
+
+    // Since OpenGL is not a library, it is a specification, it is the programmer's
+    // responsibility to find the address of each function used from opengl. Instead of
+    // loading each function before every usage, we could just load it automatically by
+    // by providing the following closure to "gl" library.
+    let load_callback = |s| {
+        sdl_context.video().unwrap().gl_get_proc_address(s) as *const c_void
+    };
+    gl::load_with(load_callback);
+    gl::Viewport::load_with(load_callback);
+    unsafe { gl::Viewport(0, 0, INITIAL_WINDOW_WIDTH as i32, INITIAL_WINDOW_HEIGHT as i32); }
 }
 
 fn main() {
+    // Initialize the SDL2 context
     let sdl_context = sdl2::init().unwrap();
+
+    // Initialize & Open a new window
     let window = open_window(&sdl_context);
+
+    initialize_gl(&sdl_context);
+
+    // Start the event SDL event loop which takes care responding to buttons,
+    // clicks, and other events.
     start_event_loop(&sdl_context, &window);
 }
